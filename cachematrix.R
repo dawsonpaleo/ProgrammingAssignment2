8cd2d5aa9c2ceeea8b7dce5b5a96b2139ab488c5
dawsonpaleo <dawsonpaleo@gmail.com>
2014-11-21 19:44:52 -0600

    Initial edit of cachemean.  Function works, but needs comments.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invertmatrix <- NULL
+        set <- function(y) {
+        x <<- y
+        invertmatrix <<- NULL
+  }
+        get <- function() x
+        setinvert <- function(solve) invertmatrix <<- solve
+        getinvert <- function() invertmatrix
+        list(set = set, get = get,
+        setinvert = setinvert,
+        getinvert = getinvert)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invertmatrix <- x$getinvert()
+        if(!is.null(invertmatrix)) {
+                message("getting cached data")
+                return(invertmatrix)
+        }
+        data <- x$get()
+        invertmatrix <- solve(data, ...)
+        x$setinvert(invertmatrix)
+        invertmatrix
 }
